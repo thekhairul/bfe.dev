1dcc1b9f5f256e38eec070aded606b41b4442b73
thekhairul <thekhairul.office@gmail.com>
2022-11-12 13:39:39 +0600

    implement curry with placeholder

--- Javascript/questions.js
+++ Javascript/questions.js
@@ -9,3 +9,14 @@ export function curry(fn) {
 }
 
 // 2. implement curry with placeholder
+export function curryWithPlaceholder(fn) {
+  if (typeof fn !== 'function') return new Error(`Expected function as a parameter. Found: ${typeof fn}`)
+    return function curryied(...args) {
+        if (args.length >= fn.length && !args.slice(0,fn.length).includes(curryWithPlaceholder.placeholder)) return fn.apply(this, args);
+        return (...args2) => {
+            const replacedArgs = args.includes(curryWithPlaceholder.placeholder) ? args.map(el => el === curryWithPlaceholder.placeholder && args2.length ? args2.shift() : el) : [...args, ...args2];
+            return curryied(...replacedArgs);
+        };
+    }
+}
+curryWithPlaceholder.placeholder = Symbol()
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
+import javascriptLogo from './javascript.svg'
+import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <div>
